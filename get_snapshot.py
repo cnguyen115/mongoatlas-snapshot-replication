3b6d1c052aa961909fe62bdd00846a599bc49cd8
Chris Nguyen <cnguyen@mfour.com>
2018-06-12 10:21:52 -0700

    Change string concatenation

--- get_snapshot.py
+++ get_snapshot.py
@@ -4,8 +4,8 @@ import yaml
 
 def get_snapshot_id(username, password, group_id, cluster_name):
     """Return snapshot id calling Mongo Atlas API: Get All Snapshots"""
-    url = 'https://cloud.mongodb.com/api/atlas/v1.0/groups/' + group_id + \
-        '/clusters/' + cluster_name + '/snapshots?itemsPerPage=1'
+    url = ("https://cloud.mongodb.com/api/atlas/v1.0/groups/{}/clusters/{}/"
+           "snapshots?itemsPerPage=1".format(group_id, cluster_name))
 
     r = requests.get(url, auth=HTTPDigestAuth(username, password))
     r.raise_for_status()
@@ -21,8 +21,8 @@ def restore_jobs(username, password, group_id, cluster_name, snapshot_id):
       },
       "snapshotId" : snapshot_id
     }
-    url = 'https://cloud.mongodb.com/api/atlas/v1.0/groups/' + group_id +  \
-        '/clusters/' + cluster_name + '/restoreJobs?pretty=true'
+    url = ("https://cloud.mongodb.com/api/atlas/v1.0/groups/{}/clusters/{}/"
+           "restoreJobs".format(group_id, cluster_name))
 
     r = requests.post(url, json=payload,
                       auth=HTTPDigestAuth(username, password))
